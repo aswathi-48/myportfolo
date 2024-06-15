c1cdf47f8ed9b90b490f8bb176a1b9ffbd7586da
aswathi-48 <aswathik482@gmail.com>
2024-06-15 16:11:08 +0530

    solved issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
-import styles from "./App.Module.css"
 import About from "./Component/About/About"
 import Contact from "./Component/Contact/Contact"
 import Experience from "./Component/Experience/Experience"
 import Hero from "./Component/Hero/Hero"
 import Navbar from "./Component/Navbar/Navbar"
 import Project from "./Component/Projects/Project"
+import styles from "./App.module.css"
 
 function App() {
 
